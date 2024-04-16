1504dbd90e6c6d8fead78aba2c14445cb5f5f2e5
phanindra Reddy <phani.vaj16@gmail.com>
2024-04-16 16:47:58 +0530

    latest changes

--- 11-installations.sh
+++ 11-installations.sh
@@ -4,11 +4,13 @@ username=$(id -u)
 if [ $username -ne 0 ]
 then
 echo "please use root user acces for the installation"
-exit 1
+exit 1 
 else
 echo "you are a root user"
 fi
 
 dnf install mysql -y
 
+ 
+echo "is script still proceeding"
 
