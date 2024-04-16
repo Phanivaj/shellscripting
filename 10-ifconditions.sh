3ba2ec4e82949dadfe2f0601ecbeff1c0f672eb2
phanindra Reddy <phani.vaj16@gmail.com>
2024-04-16 16:07:46 +0530

    latest changes

--- 10-ifconditions.sh
+++ 10-ifconditions.sh
@@ -5,9 +5,11 @@ read fnumber
 echo "enter second number::"
 read snumber
 
-if [ $fnumber -gt $snumber ] then
+if [ $fnumber -gt $snumber ] 
+then
 echo "first number is greater than second number"
-elif [ $fnumber -lt $snumber ] then
+elif [ $fnumber -lt $snumber ] 
+then
 echo "first number is lesser than second number"
 else 
 echo "Both the numbers are equal"
