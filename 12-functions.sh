02017127998f65779626797441f66fc55991a4c7
phanindra Reddy <phani.vaj16@gmail.com>
2024-04-18 16:33:18 +0530

    Latest changes

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 username=$(id -u)
+userid=$(whoami)
 date=$(date +%F-%H-%M-%S)
 R=\e[32m
 G=\e[33m
-echo $UNAME Executing $0
+echo $userid Executing $0
 echo "Execution started at $date"
 if [ $username -ne 0 ]
 then
@@ -15,7 +16,7 @@ echo "you are a root user"
 fi
 
 validate(){
-    if [$1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
     echo $2 ....failed
     exit 1
